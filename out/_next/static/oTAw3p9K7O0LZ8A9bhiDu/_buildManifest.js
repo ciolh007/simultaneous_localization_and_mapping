4b45c77fe6af00c9df6bbcaa3426dddda5675b73
Brunela Dalama <jkbrune100@outlook.com>
2021-09-24 14:52:44 +0800

    deploy Next.js to gh-pages

--- out/_next/static/oTAw3p9K7O0LZ8A9bhiDu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-924eeecd18442027397a.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],"/about":["static/chunks/pages/about-33a4305cc71fd64085b2.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
