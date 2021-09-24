586809820deb8cabf792b7d7b7afdea94625e951
Brunela Dalama <jkbrune100@outlook.com>
2021-09-24 15:33:42 +0800

    mdx

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? '/artificial_intelligence' : '',
+  'process.env.BACKEND_URL': prod ? '/reinforcement_learning' : ''
 }
--- next.config.js
+++ next.config.js
@@ -2,8 +2,18 @@
 // So, we write it in vanilla JS
 // (But you could use ES2015 features supported by your Node.js version)
 
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+})
+
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'mdx'],
+})
+
 const debug = process.env.NODE_ENV !== 'production'
 
-module.exports = {
-  assetPrefix: !debug ? '/artificial_intelligence/' : '',
-}
+// module.exports = {
+//   assetPrefix: !debug ? '/reinforcement_learning/' : '',
+// }
+
+
